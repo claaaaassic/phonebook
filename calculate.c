70cb15fc74bb78e7f18c36603f168d2a8f6b9de4
Weinux <weiiawia@gmail>
2017-05-02 02:03:42 +0800

    For create performance comparision plot

--- calculate.c
+++ calculate.c
@@ -43,10 +43,11 @@ int main(void)
         opt_sum_a += opt_a;
         opt_sum_f += opt_f;
     }
+    fclose(fp);
 
     fp = fopen("hash.txt", "r");
     if (!fp) {
-        fp = fopen("opt.txt", "r");
+        fp = fopen("orig.txt", "r");
         if (!fp) {
             printf("ERROR opening input file hash.txt\n");
             exit(0);
@@ -63,9 +64,32 @@ int main(void)
         hash_sum_a += hash_a;
         hash_sum_f += hash_f;
     }
-    fprintf(output, "append() %lf %lf %lf\n",orig_sum_a / 100.0, opt_sum_a / 100.0, hash_sum_a / 100.0);
-    fprintf(output, "findName() %lf %lf %lf", orig_sum_f / 100.0, opt_sum_f / 100.0, hash_sum_f);
-    fclose(output);
     fclose(fp);
+
+    fp = fopen("bst.txt", "r");
+    if (!fp) {
+        fp = fopen("orig.txt", "r");
+        if (!fp) {
+            printf("ERROR opening input file bst.txt\n");
+            exit(0);
+        }
+    }
+    double bst_sum_a = 0.0, bst_sum_f = 0.0, bst_a, bst_f;
+    for (i = 0; i < 100; i++) {
+        if (feof(fp)) {
+            printf("ERROR: You need 100 datum instead of %d\n", i);
+            printf("run 'make run' longer to get enough information\n\n");
+            exit(0);
+        }
+        fscanf(fp, "%s %s %lf %lf\n", append, find, &bst_a, &bst_f);
+        bst_sum_a += bst_a;
+        bst_sum_f += bst_f;
+    }
+    fclose(fp);
+
+
+    fprintf(output, "append() %lf %lf %lf %lf \n",orig_sum_a / 100.0, opt_sum_a / 100.0, hash_sum_a / 100.0, bst_sum_a / 100.0 );
+    fprintf(output, "findName() %lf %lf %lf %lf ", orig_sum_f / 100.0, opt_sum_f / 100.0, hash_sum_f / 100.0, bst_sum_f / 100.0 );
+    fclose(output);
     return 0;
 }
